668a3696d0beb1ea79d34500ae86a8b90243aee5
Vell <137341548+VellVoid@users.noreply.github.com>
2023-06-23 03:04:29 +0200

    Update quantum_circuit_w_logging.py
    Script with logging functions

--- quantum_circuit_w_logging.py
+++ quantum_circuit_w_logging.py
@@ -1,1 +1,11 @@
 comming soon...
+
+# -----------------------------------------------------------------------
+# quantum_circuit_w_logging.py
+# Version: 0.0
+# Author: Vell Void
+# GitHub: https://github.com/VellVoid
+# Twitter: https://twitter.com/VellVoid
+# This script will add a level of logging to the software 
+# so you can save your results
+# -----------------------------------------------------------------------
